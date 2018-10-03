587e93f23584ade7f1123dad8a266d8c8848038a
Krokky <875594067@qq.com>
2018-10-03 13:54:43 +0800

    1003

---
+++ leetcode-algorithms/008. String to Integer (atoi/Solution.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int myAtoi(String str) {
+        int i = 0;
+        int flag = 0;
+        double count = 0;
+        if (str.length() == 0) {
+            return 0;
+        } else if (str.charAt(0) == '-') {
+            flag = -1;
+            i++;
+        } else if (str.charAt(0) == '+') {
+            i++;
+        }
+        for (; i < str.length(); i++) {
+            if (Character.isDigit(str.charAt(i))) {
+                count = count * 10 + str.charAt(i) - '0';
+            } else {
+                break;
+            }
+        }
+
+        if (-1 == flag) {
+            count = count * (-1);
+            if (count < Integer.MIN_VALUE) {
+                return Integer.MIN_VALUE;
+            } else {
+                return (int) count;
+            }
+        } else {
+            if (count > Integer.MAX_VALUE) {
+                return Integer.MAX_VALUE;
+            } else {
+                return (int) count;
+            }
+        }
+    }
+}
